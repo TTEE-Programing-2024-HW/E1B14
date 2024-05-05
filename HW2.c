722e7e5b82baa1c0275fe6605d80997797966ec1
E1B14 <u11202214@o365.ttu.edu.tw>
2024-05-05 14:32:17 +0800

    你好

---
+++ HW2.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// 步驟1：顯示個人風格的畫面
+void step1() {
+    printf("E1B14鄭力允                                                    \n");
+    printf("             *                                                 \n");
+    printf("            * **                                               \n");
+    printf("            *   **                                             \n");
+    printf("           **    ***                                           \n");
+    printf("           *       **                                          \n");
+    printf("           *       ***                                         \n");
+    printf("           *     ***************                               \n");
+    printf("           *   **         * *******                            \n");
+    printf("           *****                  *****                        \n");
+    printf("           ***                        ******                   \n");
+    printf("           **                               ***                \n");
+    printf("          **     ******                       *****            \n");
+    printf("         **     *  *  **                       **  *****       \n");
+    printf("        **      * ******             ****       **      ****   \n");
+    printf("       **       *  *****           *********     **      ***** \n");
+    printf("      **         *****  *******   ****  ** **     **   **      \n");
+    printf("     **                ********    ******  ***     *****       \n");
+    printf("    **                   *******    ****  **         **        \n");
+    printf("    *                     ******      *****          ***       \n");
+    printf("    *                    ***                          **       \n");
+    printf("    ***               **** **                         **       \n");
+    printf("     **            ***      ***                       *        \n");
+    printf("      **                      *                       *        \n");
+    printf("       **                                            **        \n");
+    printf("        *                                      --   **         \n");
+    printf("        *                                  ---   **            \n");
+    printf("       **                            ------      **            \n");
+    printf("      ***                    --------             *            \n");
+    printf("     ****                                         *            \n");
+    printf("    ****                                          **           \n");
+    printf("   ****                                             ********   \n");
+    printf(" **** **                                                  ***  \n");
+    system("PAUSE");
+    system("cls");
+}
+
+// 步驟2：輸入並檢查密碼是否正確
+int check() {
+    int pass;
+    int time = 0; // 計算輸入密碼的次數
+
+    do {
+        printf("請輸入4個數字的密碼：");
+        scanf("%d", &pass);
+        // 檢查密碼是否正確
+        if(pass == 2024) {
+            printf("密碼正確！歡迎使用本程式。\n");
+            return 1;
+        }
+		else {
+            time++;
+            printf("警告：密碼錯誤%d次\n",time);
+            if(time==3) {
+                printf("輸入錯誤超過三次，程式即將結束。\n");
+                return 0;
+            }
+        }
+    }while(time < 3);
+
+    return 0;
+}
+
+int main() {
+    // 顯示個人風格的畫面
+    step1();
+
+    // 輸入密碼並檢查是否正確
+    if(!check()) {
+        return 0;
+    }
+
+}
