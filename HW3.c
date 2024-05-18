67b0540ec900edf16e6dff6a3911b4e9858c203b
E1B14 <u11202214@o365.ttu.edu.tw>
2024-05-18 17:40:37 +0800

    a部分有亂碼

--- HW3.c
+++ HW3.c
@@ -81,13 +81,12 @@ void step2(){
     printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
 }
 
-// 步驟3：
-void step3(char seat[9][9]) {
+// 步驟3：顯示座位表
+void step3(char seat[9][9]){
     printf("座位表：\n");
-    int i;
+    int i,j;
     for(i=0;i<9;i++){
-    	int j;
-        for (j=0;j<9;j++){
+        for(j=0;j<9;j++) {
             printf("%c",seat[i][j]);
         }
         printf("\n");
@@ -96,62 +95,66 @@ void step3(char seat[9][9]) {
     system("pause"); // 等待用戶按下任意鍵
 }
 
-
 // 隨機產生已被預訂的座位
-void rand_seat(char seat[9][9]) {
+void rand_seat(char seat[9][9]){
     int co=0;
-    srand(123); // 如果未用此行rand的值會在開始時就決定為定值 
-
+    // 使用 rand 函數的返回值作為種子
+    srand(123);
     // 隨機產生十個已被預訂的座位
     while(co<10){
-        int x=rand()%9; // 生成隨機行號
-        int y=rand()%9; // 生成隨機列號
+        int x=rand()%9;// 生成隨機行號
+        int y=rand()%9;// 生成隨機列號
         if (seat[x][y]!='*'){
-            seat[x][y]='*'; // 標記座位為已預訂
+            seat[x][y]='*';// 標記座位為已預訂
             co++;
         }
     }
 }
 
+
 int main() {
     char seat[9][9]; // 宣告座位表
     char choice; // 用於存儲用戶的選擇
 
-     // 顯示個人風格的畫面
+    // 顯示個人風格的畫面
     step1();
 
     // 輸入密碼並檢查是否正確
-    if(!check()) {
+    if (!check()) {
         return 0;
     }
-        // 顯示主選單（步驟2）
-        step2();
 
-        // 詢問用戶的選擇
-        printf("請選擇：");
-        fflush(stdin);
-        scanf("%c",&choice);
+    // 隨機產生已被預訂的座位
+    rand_seat(seat);
 
-        // 根據用戶的選擇執行相應的操作
-        switch (choice) {
-            case 'a':
-                // 顯示可用座位（步驟3）
-                step3(seat);
-                break;
-            case 'b':
-                // 安排座位
-                
-                break;
-            case 'c':
-                // 手動選擇座位
-                
-                break;
-            case 'd':
-                // 退出程式
-                return 0;
-            default:
-                printf("錯誤選項。\n");
-                break;
-        }
-		return 0;
+    // 顯示主選單（步驟2）
+    step2();
+
+    // 詢問用戶的選擇
+    printf("請選擇：");
+    fflush(stdin);
+    scanf("%c", &choice);
+
+    // 根據用戶的選擇執行相應的操作
+    switch (choice) {
+        case 'a':
+            // 顯示可用座位（步驟3）
+            step3(seat);
+            break;
+        case 'b':
+            // 安排座位
+            break;
+        case 'c':
+            // 手動選擇座位
+            break;
+        case 'd':
+            // 退出程式
+            return 0;
+        default:
+            printf("錯誤選項。\n");
+            break;
     }
+
+    return 0;
+}
+
