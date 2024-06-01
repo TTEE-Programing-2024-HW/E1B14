ad8ab67eca2cdfd5468f9181fee24cbaf2442e0d
E1B14 <u11202214@o365.ttu.edu.tw>
2024-06-01 23:10:51 +0800

    將一樣的地方(step1/step2)複製

---
+++ hw4.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+// 步驟1：顯示個人風格的畫面
+void step1(){
+	printf("E1B14鄭力允                                                    \n");
+	printf("             *                                                 \n");
+	printf("            * **                                               \n");
+	printf("            *   **                                             \n");
+	printf("           **    ***                                           \n");
+	printf("           *       **                                          \n");
+	printf("           *       ***                                         \n");
+	printf("           *     ***************                               \n");
+	printf("           *   **         * *******                            \n");
+	printf("           *****                  *****                        \n");
+	printf("           ***                        ******                   \n");
+	printf("           **                               ***                \n");
+	printf("          **     ******                       *****            \n");
+	printf("         **     *  *  **                       **  *****       \n");
+	printf("        **      * ******             ****       **      ****   \n");
+	printf("       **       *  *****           *********     **      ***** \n");
+	printf("      **         *****  *******   ****  ** **     **   **      \n");
+	printf("     **                ********    ******  ***     *****       \n");
+	printf("    **                   *******    ****  **         **        \n");
+	printf("    *                     ******      *****          ***       \n");
+	printf("    *                    ***                          **       \n");
+	printf("    ***               **** **                         **       \n");
+	printf("     **            ***      ***                       *        \n");
+	printf("      **                      *                       *        \n");
+	printf("       **                                            **        \n");
+	printf("        *                                      --   **         \n");
+	printf("        *                                  ---   **            \n");
+	printf("       **                            ------      **            \n");
+	printf("      ***                    --------             *            \n");
+	printf("     ****                                         *            \n");
+	printf("    ****                                          **           \n");
+	printf("   ****                                             ********   \n");
+	printf(" **** **                                                  ***  \n");
+	system("PAUSE");
+	system("cls");
+}
+
+
+// 輸入並檢查密碼是否正確
+int check(){
+    int pass;
+    int time=0; // 計算輸入密碼的次數
+    do{
+        printf("請輸入4個數字的密碼：");
+        fflush(stdin);
+        scanf("%d",&pass);
+        // 檢查密碼是否正確
+        if(pass==2024) {
+            printf("密碼正確！歡迎使用本程式。\n");
+            return 1;
+        }
+		else {
+            time++;
+            printf("警告：密碼錯誤%d次\n",time);
+            if(time==3) {
+                printf("輸入錯誤超過三次，程式即將結束。\n");
+                return 0;
+            }
+        }
+    }while(time<3);
+    return 0;
+}
+
+// 步驟2：螢幕主選單
+void step3(){
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+    printf("*         Welcome to the Main         *\n");
+    printf("*                Menu                 *\n");
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+    printf("*       a. Enter student grades       *\n");
+    printf("*       b. Display student grades     *\n");
+    printf("*       c. Search for student grades  *\n");
+    printf("*       d. Grade ranking              *\n");
+    printf("*       e. Exit system                *\n");
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+}
+
+
